ae3a89541ec3160ca076ae714cab2f3a9654cc7f
hihilong <hyelong20630@gmail.com>
2025-12-22 03:50:14 +0900

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    // Expose API KEY to server side only
-    API_KEY: process.env.API_KEY,
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
